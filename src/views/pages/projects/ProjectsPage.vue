681b6da6aa4aa074d10a6e87675c5ed092190e00
unknown <mekan.developer@gmail.com>
2026-01-05 19:27:18 +0500

    fix: comentirowal translateProjectStatus funcsiyu na ProjectPage.vue

--- src/views/pages/projects/ProjectsPage.vue
+++ src/views/pages/projects/ProjectsPage.vue
@@ -225,7 +225,7 @@ export default {
         eventBus.off('cache:invalidate', this.handleCacheInvalidate);
     },
     methods: {
-        translateProjectStatus,
+        // translateProjectStatus,
         itemMapper(i, c) {
             switch (c) {
                 case 'users':
