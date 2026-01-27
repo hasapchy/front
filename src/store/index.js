978ac5d8536a0fb29973b89ef564fd19e8ad8aff
Mekan <mekan.developer@gmail.com>
2026-01-27 13:23:35 +0500

    rollback permitions

--- src/store/index.js
+++ src/store/index.js
@@ -1461,7 +1461,7 @@ const store = createStore({
           to: "/basement-orders",
           icon: "fas fa-cart-arrow-down mr-2",
           label: "orders_basement",
-          permission: "orders_basement_view",
+          permission: "orders_view",
           basementOnly: true,
         },
         {
